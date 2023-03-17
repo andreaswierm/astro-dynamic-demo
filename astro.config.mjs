3f3cd706a7405f3254742248c3ff87b41695a5f7
Andreas Wiermann Casas <andreas.w.dev@gmail.com>
2023-03-17 09:24:30 -0300

    adds react + dynamic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,21 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  integrations: [react()]
+});
